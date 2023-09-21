6c6209e4c498b311f9f4f1aa1b4666d3c7da00a4
philipjpark <philipparknyc@gmail.com>
2023-09-20 23:15:24 -0400

    Set up Express Server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const app = express()
+
+app.use('/public', express.static('./public'));
+app.use('/scripts', express.static('./public/scripts'))
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">Listicle API</h1>')
+})
+
+// const PORT = process.env.PORT || 3001
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server listening on http://localhost:${PORT}`)
+})
