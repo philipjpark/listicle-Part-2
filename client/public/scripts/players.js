25a35e731cf704d7700672257cd965e0a6acfadb
philipjpark <philipparknyc@gmail.com>
2023-09-22 02:56:46 -0400

    Step 6: Dynamically display player  data on the home page

---
+++ client/public/scripts/players.js
@@ -0,0 +1,60 @@
+const renderPlayers = async () => {
+
+    const response = await fetch('/players')
+    const data = await response.json()
+
+    const mainContent = document.getElementById('main-content')
+
+    if (data) {
+
+        data.map(player => {
+            const card = document.createElement('div')
+            card.classList.add('card')
+
+            const topContainer = document.createElement('div')
+            topContainer.classList.add('top-container')
+
+            const bottomContainer = document.createElement('div')
+            bottomContainer.classList.add('bottom-container')
+
+            topContainer.style.backgroundImage = `url(${player.image})`
+
+            const name = document.createElement('h3')
+            name.textContent = player.name
+            bottomContainer.appendChild(name)
+
+            const height = document.createElement('p')
+            height.textContent = 'Height: ' + player.height
+            bottomContainer.appendChild(height)
+
+            const weight = document.createElement('p')
+            weight.textContent = 'Weight: ' + player.weight
+            bottomContainer.appendChild(weight)
+
+            const position = document.createElement('p')
+            position.textContent = 'Position: ' + player.position
+            bottomContainer.appendChild(position)
+
+            const club = document.createElement('p')
+            club.textContent = 'Club: ' + player.club
+            bottomContainer.appendChild(club)
+
+            const link = document.createElement('a')
+            link.textContent = 'Read More >'
+            link.setAttribute('role', 'button')
+            link.href = `/players/${player.id}`
+            bottomContainer.appendChild(link)
+
+            card.appendChild(topContainer)
+            card.appendChild(bottomContainer) 
+            mainContent.appendChild(card)
+        })
+    }
+    else {
+        const message = document.createElement('h2')
+        message.textContent = 'No Players Available 😞'
+        mainContent.appendChild(message)
+    }
+}
+
+renderPlayers()
