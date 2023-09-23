b8f6aec48258119457b489f42c3f30b808e8dc79
philipjpark <philipparknyc@gmail.com>
2023-09-23 01:46:05 -0400

    Updated player images

--- server/data/players.js
+++ server/data/players.js
@@ -7,7 +7,7 @@ const playerData = [
       "position": "Forward",
       "club": "Man City",
       "confederation": "UEFA",
-      "image": "erling_haaland.jpg",
+      "image": "https://upload.wikimedia.org/wikipedia/commons/6/6e/Erling_Haaland_2023_%28cropped-v2%29.jpg",
       "submittedBy": "Sasha",
       "submittedOn": "2022-09-04T14:48:00"
     },
@@ -19,7 +19,7 @@ const playerData = [
       "position": "Midfielder",
       "club": "AC Milan",
       "confederation": "CONCACAF",
-      "image": "christian_pulisic.jpg",
+      "image": "https://upload.wikimedia.org/wikipedia/commons/f/f3/USMNT_vs._Trinidad_and_Tobago_%2848125059622%29_%28cropped%29.jpg",
       "submittedBy": "John",
       "submittedOn": "2022-09-05T10:15:00"
     },
@@ -31,7 +31,7 @@ const playerData = [
       "position": "Forward",
       "club": "Inter Miami",
       "confederation": "CONMEBOL",
-      "image": "lionel_messi.jpg",
+      "image": "https://upload.wikimedia.org/wikipedia/commons/b/b4/Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg",
       "submittedBy": "Maria",
       "submittedOn": "2022-09-06T16:30:00"
     },
@@ -43,7 +43,7 @@ const playerData = [
       "position": "Forward",
       "club": "Tottenham Hotspur",
       "confederation": "AFC",
-      "image": "son_heung_min.jpg",
+      "image": "https://upload.wikimedia.org/wikipedia/commons/c/c1/Team_Korea_Russia_WorldCup_02_%28cropped%29.png",
       "submittedBy": "David",
       "submittedOn": "2022-09-07T12:20:00"
     },
@@ -55,7 +55,7 @@ const playerData = [
       "position": "Forward",
       "club": "Liverpool",
       "confederation": "CAF",
-      "image": "mohammed_salah.jpg",
+      "image": "https://upload.wikimedia.org/wikipedia/commons/4/4a/Mohamed_Salah_2018.jpg",
       "submittedBy": "Olivia",
       "submittedOn": "2022-09-08T09:55:00"
     },
@@ -67,7 +67,7 @@ const playerData = [
       "position": "Forward",
       "club": "Nottingham Forest",
       "confederation": "OFC",
-      "image": "chris_wood.jpg",
+      "image": "https://upload.wikimedia.org/wikipedia/commons/9/95/Russia-New_Zeland_%2817%29.jpg",
       "submittedBy": "Michael",
       "submittedOn": "2022-09-09T15:10:00"
     }
